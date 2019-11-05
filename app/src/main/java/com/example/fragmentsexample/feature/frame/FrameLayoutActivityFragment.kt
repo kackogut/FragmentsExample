39956ca6d1d44ba5dea0f963042a60845d09ac17
Kacper Kogut <kacper.kogut@netguru.com>
2019-11-05 09:48:19 +0100

    Add fragments transitions

--- app/src/main/java/com/example/fragmentsexample/feature/frame/FrameLayoutActivityFragment.kt
+++ app/src/main/java/com/example/fragmentsexample/feature/frame/FrameLayoutActivityFragment.kt
@@ -6,27 +6,31 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.fragmentsexample.R
-import kotlinx.android.synthetic.main.fragment_frame_layout_first.*
+import com.example.fragmentsexample.utils.Utils
+import kotlinx.android.synthetic.main.fragment_frame_layout.*
 
 /**
  * A placeholder fragment containing a simple view.
  */
 class FrameLayoutActivityFragment : Fragment() {
 
+    private val backgroundColor = Utils.getRandomColor()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_frame_layout_first, container, false)
+        return inflater.inflate(R.layout.fragment_frame_layout, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        fragmentFrameConstraintLayout.setBackgroundColor(backgroundColor)
         fragmentFrameFirstButton.setOnClickListener {
-            val fragment = FrameLayoutActivitySecondFragment()
             fragmentManager?.beginTransaction()
-                ?.replace(R.id.fragment, fragment)
+                ?.setCustomAnimations(R.anim.enter_from_left, R.anim.exit_to_right, R.anim.enter_from_right, R.anim.exit_to_left)
+                ?.replace(R.id.fragment, FrameLayoutActivityFragment())
                 ?.addToBackStack(null)
                 ?.commit()
         }
